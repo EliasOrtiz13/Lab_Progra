0a150fd44ce1c2c9904ab87bb206b47cec6aa80b
Elias Ortiz <t1053600521@unitru.edu.pe>
2022-10-26 10:17:53 -0500

    Lab prograII

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.get('/', (req, res) => {
 //Muestra los datos de data.js
 app.get('/api/clientes', (req, res) => {
     res
-        .status(200)    //OK
+        .status(200) 
         .json({ success: true, data: clientes}) 
 })
 
 //Pagina particular para cada elemento
 app.get('/api/clientes/:clientID', (req, res) => {
-    console.log(req.params)
+    console.log(req.params) //obtener informacion de un elemento
     const {clientID} = req.params
     const singleClient = clientes.find(
         client => client.id === Number(clientID)
@@ -38,30 +38,30 @@ app.post('/api/clientes', (req, res) => {
 
     if(!id || !dni || !apellido || !nombre || !edad){ //Verificar que se completaron los datos
         return res
-            .status(400)    //Bad request
+            .status(400)    //Peticion mala
             .json({ succes: false, msg: 'Proveer el id, dni, apellido, nombre, y la edad del cliente'})  
     }
     else
-    if(typeof id !== "number" || typeof dni !== "number" || typeof edad !== "number"){  //Si se ingresa una str en id, dni o edad
+    if(typeof id !== "number" || typeof dni !== "number" || typeof edad !== "number"){  //Si se ingresa un str en id, dni o edad
         res
             .send({succes: false, msg: 'Ingrese solo números para el id, dni y la edad'}) 
     }
     res
-        .status(201)    //Created
-        .send( {succes: true, data: [...clientes, {id, dni, apellido, nombre, edad}] })  //El proceso exitoso y agrega un nuevo cliente
+        .status(201) //Estado de exito
+        .send( {succes: true, data: [...clientes, {id, dni, apellido, nombre, edad}] })  //Proceso exitoso y agrega un nuevo cliente
 })
 
 
 app.put('/api/clientes/:id', (req, res) => {
-    const {id} = req.params
-    const {dni, apellido, nombre, edad} = req.body //Se asigna el valor que se está ingresando
-    const client = clientes.find(
-        (client) => client.id === Number(id) //Busca el id dentro de data.js
+    const {id} = req.params   //obtener informacion de un elemento
+    const {dni, apellido, nombre, edad} = req.body //Se obtiene los valores que están ingresando
+    const client = clientes.find( //Busca el id en data.js
+        (client) => client.id === Number(id) 
     )
-    if(!client){    //Se ejecuta si no encuentra algun cliente con ese id
+    if(!client){    //Si no encuentra algun cliente con ese id
         return res
-            .status(400)    //Bad request
-            .json({ succes: false, msg: `No fue encontrado el cliente con id: ${id}`}) //El proceso no se logro ejecutar y envia un mensaje
+            .status(400)  //Peticion mala
+            .json({ succes: false, msg: `No fue encontrado el cliente con id: ${id}`}) //Proceso fallido y envia un mensaje
     }
     const newClient = clientes.map(client => {
         if (client.id === Number(id)) {
@@ -73,13 +73,13 @@ app.put('/api/clientes/:id', (req, res) => {
         return client
     })
     res
-        .status(201)    //Created
+        .status(201)  //Estado de exito
         .send( {succes: true, data: newClient})
 })
 
 
 app.delete('/api/clientes/:id', (req, res) => {
-    const {id} = req.params
+    const {id} = req.params  
     const client = clientes.find(
         (client) => client.id === Number(id)
     )
@@ -88,18 +88,18 @@ app.delete('/api/clientes/:id', (req, res) => {
             .status(400)
             .json({succes: false, msg: `El cliente no posee id: ${id}`})
     }
-    const newClient = clientes.filter(     //filtrar
+    const newClient = clientes.filter(  //filtrar
         client => client.id !== Number(id)
     )
     return res
-        .status(201)    //Created
+        .status(201) //Estado de exito
         .send({succes: true, data: newClient})
 })
 
 
 app.all('*', (req, res) => {
     res
-        .status(404)
+        .status(404) //Error
         .send('<h1>NO ENCONTRADO</h1>') //Pagina por defecto cuando no se encuentra la direccion url
 })
 
